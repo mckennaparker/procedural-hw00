a2e91f1c0d0a4fbe6741c10798806a69c20dbe2b
McKenna Parker <152559424+mckennaparker@users.noreply.github.com>
2025-09-08 23:42:33 -0400

    Fix formatting and improve noise calculation in shader

--- src/shaders/custom-frag.glsl
+++ src/shaders/custom-frag.glsl
@@ -77,11 +77,11 @@ float worley(vec3 p, float scale){
         for(float y = -1.0; y <=1.0; y++){
             for(float x = -1.0; x <= 1.0; x++){
                 vec3 coord = vec3(x,y,z);
-                vec3 rId = random(mod(id+coord,scale))*0.5+0.5;
+                vec3 rId = random(mod(id + coord, scale)) * 0.5 + 0.5;
 
                 vec3 r = coord + rId - fd; 
 
-                float d = dot(r,r);
+                float d = dot(r, r);
 
                 if(d < minDist){
                     minDist = d;
@@ -101,7 +101,7 @@ float getBias(float x, float bias)
 
 void main()
 {
-    float noise = worley(fs_Pos.xyz + 2.f * getBias(cos(u_Time/ 10.f), 0.2), worley(fs_Pos.xyz + u_Time / 10.f, 2.f));
+    float noise = worley(fs_Pos.xyz + 2.0 * getBias(cos(u_Time/ 10.f), 0.2), worley(fs_Pos.xyz + u_Time / 10.f, 2.0));
 
     vec4 diffuseColor =  vec4(noise * 0.5 + 0.2 * sin(u_Time), noise * 0.3 + 0.3 * cos(u_Time), noise * 0.7 + 0.5 * sin(u_Time), 1) + u_Color;
 
